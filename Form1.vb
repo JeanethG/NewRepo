0eed95347cdd4556c534a77156c8ba2198ed60fa
jcrgetalado@gmail.com <jcrgetalado@gmail.com>
2022-10-18 10:25:15 +0800

    forms of program1 pushed

---
+++ Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+
+        MsgBox("Hello Mel")
+        MsgBox("Hindi ko masundan yung tutorial ng Windows na binigay mo")
+        MsgBox("Humanap na lang ako sa Yt")
+        MsgBox("Ito pa lang nagawa ko, tama ba ito?")
+    End Sub
+End Class
